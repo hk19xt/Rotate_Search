f7120faf857566f128175ec46e384cc11c0aeb4d
Hyejin Kim <81273823+hk19xt@users.noreply.github.com>
2021-09-14 13:51:43 -0400

    Update Rotate_Search.java

--- Rotate_Search/src/Rotate_Search/Rotate_Search.java
+++ Rotate_Search/src/Rotate_Search/Rotate_Search.java
@@ -1,37 +1,35 @@
 package Rotate_Search;
 
-/*This class is the implementation class to find the minimum value in the array.
+/*This class is the implementation class to find the target value in the array.
 * This implementation will be using the binary search by sorted array.
 * The input values and the target value are inserted by users in the main class. */
 
 public class Rotate_Search {
+    
     public int calculation(int[] inputs, int target){
+        
         int initialPosition = 0;
         int lastPosition = inputs.length - 1;
 
-        //In order to find the minimum value, we should consider the five cases in total.
+        //In order to find the minimum value, we should consider the three cases in total.
         while(initialPosition<=lastPosition){
             int middlePosition = (initialPosition + lastPosition)/2;
-            //Case 2
+            
+            //Case 1
             if(inputs[initialPosition]<=inputs[middlePosition]){
-//                if(target>inputs[middlePosition]&&target<=inputs[lastPosition]){
-//                    initialPosition = middlePosition+1;
-//                }
+
                 if(target>=inputs[initialPosition] && target<inputs[middlePosition]){
                     lastPosition = middlePosition-1;
                 }else{
                     initialPosition = middlePosition+1;
                 }
             }
-            //Case 1
+            //Case 2
             if(inputs[middlePosition]==target){
                 return target;
             }
             //Case 3
             if(inputs[middlePosition]<=inputs[lastPosition]){
-//                if(target<inputs[middlePosition]&&target>=inputs[lastPosition]){
-//                    lastPosition = middlePosition-1;
-//                }
 
                 if(target>inputs[middlePosition]&&target<=inputs[lastPosition]){
                     initialPosition = middlePosition+1;
